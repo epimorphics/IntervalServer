31609dea24e67b2a487a11b35c7f04a5fd9fdb1f
Stuart Williams <skw@epimorphics.com>
2016-06-29 17:24:18 +0100

    Updated License headers to Apache 2.0

--- src/main/java/com/epimorphics/govData/URISets/intervalServer/ukgovcal/UkGovHalfDoc.java
+++ src/main/java/com/epimorphics/govData/URISets/intervalServer/ukgovcal/UkGovHalfDoc.java
@@ -1,18 +1,23 @@
 /******************************************************************
- * File:        UkHalfDoc.java
+ * File:        UkGovHalfDoc.java
  * Created by:  Stuart Williams
  * Created on:  13 Feb 2010
  * 
  * (c) Copyright 2010, Epimorphics Limited
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * $UkId:  $
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ * $Id:  $
  *****************************************************************/
 
 package com.epimorphics.govData.URISets.intervalServer.ukgovcal;
